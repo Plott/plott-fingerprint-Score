0eb2d8d9435d6f578966c2db971369904c3136de
Corey White <smortopahri@gmail.com>
2015-09-07 14:04:45 -0400

    Added module structure

---
+++ index.js
@@ -0,0 +1,24 @@
+/**
+ * Converts quality of RSSI signal strength to distance.
+ *
+ * @module plott/qualityDistance
+ * @category helper
+ * @param {Integer} m dBm RSSI dBm
+ * @param {Number} p maximum coverage of signal
+ * @return {Number} distance
+ * @example
+ * var distance = plott.qualityDistance(-75, 400);
+ *
+ * //=distance
+ */
+var isInteger = Number.isInteger || function(value) {
+    return typeof value === "number" &&
+           isFinite(value) &&
+           Math.floor(value) === value;
+};
+
+module.exports = function (m, p) {
+  if (!isInteger(p)) throw new Error ('Maximum coverage of signal must be an integer');
+  m = rssiQuality(m);
+  return p * (1 - m);
+};
